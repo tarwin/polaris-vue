64624f043ac79abd1b9724da33460594feb34a75
oe.sonnh <sonnh@ownego.com>
2022-04-04 21:36:28 +0700

    Check ButtonGroup slots type

--- src/components/ButtonGroup/ButtonGroup.vue
+++ src/components/ButtonGroup/ButtonGroup.vue
@@ -52,6 +52,8 @@ const itemMarkup = computed(() => {
         return;
       }
 
+      console.log(123, item);
+
       if (item.type.toString() === 'Symbol(Fragment)') {
         children.forEach(child => {
           items.push(child);
